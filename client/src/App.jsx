08873a16ff11de323a9a1c2201719164d63ca3d5
Remora Blackflag <104504762+remorablackflag@users.noreply.github.com>
2023-05-18 14:49:15 +0200

    formatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,20 @@ import "./App.scss";
 import { useState } from "react";
 
 function App() {
-  const [balance, setBalance] = useState(0);
-  const [address, setAddress] = useState("");
+    const [balance, setBalance] = useState(0);
+    const [address, setAddress] = useState("");
 
-  return (
-    <div className="app">
-      <Wallet
-        balance={balance}
-        setBalance={setBalance}
-        address={address}
-        setAddress={setAddress}
-      />
-      <Transfer setBalance={setBalance} address={address} />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Wallet
+                balance={balance}
+                setBalance={setBalance}
+                address={address}
+                setAddress={setAddress}
+            />
+            <Transfer setBalance={setBalance} address={address} />
+        </div>
+    );
 }
 
 export default App;
